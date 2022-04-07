19ea9ef4a3e72920d9293cf152c96a4678d084c5
anmaina <anmaina2001@gmail.com>
2022-04-07 11:46:21 +0200

    Update chess_board.py

--- arrays/chess_board.py
+++ arrays/chess_board.py
@@ -1,8 +1,10 @@
-n = 5
-list_of_dots = [['.']*n for i in range(n)]
+rows_colums = input().split()
+n = int(rows_colums[0])
+m = int(rows_colums[1])
+list_of_dots = [['.']*m for i in range(n)]
 for a in range(n):
-    for b in range(n):
-        if abs(a-b)%2==0:
+    for b in range(m):
+        if abs(a-b)%2!=0:
             list_of_dots[a][b] = '*'
 for j in range(n): 
     print(' '.join(list_of_dots[j]))
